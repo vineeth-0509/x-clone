e6b648486eb047e301537a9b229bd3105aefc34c
Vineeth-0509 <vineeththungani@gmail.com>
2025-01-16 19:38:42 +0530

    added and changed the functionality of the suggested users

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -51,20 +51,64 @@ export const followUnfollowUser = async (req, res) => {
   }
 };
 
+/*
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const usersFollowedByMe = await User.findById(userId).select("following");
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    // 1,2,3,4,5,6,
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByMe.following.includes(user._id)
+    );
+    const suggestedUsers = filteredUsers.slice(0, 4);
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    console.log("Error in getSuggestedUsers: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+*/
+
 export const getSuggestedUsers = async (req, res)=>{
   try {
     const userId = req.user._id;
     const usersFollowedByMe = await User.findById(userId).select("following");
-    const users = await User.aggregate([
-      {$match:{
-        _id:{$ne: userId}
-      }},
-      {$sample:{size:10}}
-    ])
-    const filteredUsers = users.filter(user=> !usersFollowedByMe.following.includes(user._id));
-    const suggestedUsers = filteredUsers.slice(0,4);
-    suggestedUsers.forEach(user=> user.password=null);
+    const suggestedUsers = await User.aggregate([
+      {
+        $match:{
+          _id:{ $ne: userId}, //exclude the current user
+          _id:{$nin: usersFollowedByMe.following}, //exclude followed users
+        },
+      },
+      {
+
+        $sample: {size: 10} //random users
+      },
+      {
+        $project:{
+          password:0, //exclude password field
+        },
+      },
+    ]);
+
+    const limitedSuggestedUsers = suggestedUsers.slice(0,4);
+    res.status(200).json(limitedSuggestedUsers);
+
   } catch (error) {
-    
+    console.error("Error in getSuggestedUsers", error.message);
+    res.status(500).json({error:"Internal server error"});
   }
 }
